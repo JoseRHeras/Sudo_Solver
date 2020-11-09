0614e9d69d5987a9363d4cd0e2322e9cacbb6e14
JRHeras <jheras@yahoo.com>
2020-11-09 13:51:07 -0600

    Minimax algorithm implementation

--- src/minimax.py
+++ src/minimax.py
@@ -5,17 +5,12 @@ import math
 def generate_all_possible_states(board):
     pass
 
-def is_terminal(board):
-
-    for i in range(len(board) - 1):
-        row = board[i]
-        for j in range (1, len(row)):
-            if row[j] == '.':
-                return False
-
-    return True
-
 def heuristic_evaluation(board, player, is_maximazing):
+    #Returns 1 in terminal state with a maximizing score
+    #Returns -1 in terminal state wiht a  minimizing score
+    #Returns None if game not in terminal state
+    #Returns 0 if game in terminal state but not winner
+
     ##Horizontal Evaluation
     for i in range(len(board) - 1):
         row = board[i]
@@ -64,7 +59,7 @@ def heuristic_evaluation(board, player, is_maximazing):
         print('cross2')
         return 1 if is_maximazing else -1
 
-    ##Is a full table
+    ##Game Board has more options. Therefore not a terminal state
     are_there_more_options = False
     for i in range(len(board) - 1):
         row = board[i]
@@ -77,20 +72,43 @@ def heuristic_evaluation(board, player, is_maximazing):
 
     return 0
 
+
+def generate_all_possible_moves(board):
+    possible_moves = []
+
+    for row in range(len(board) - 1):
+        for col in range(1, len(board)):
+            if board[row][col] == '.':
+                possible_moves.append((row, col))
+    
+    return possible_moves
+
 def min_max_tic_tac_toe(board, player, maximize):
+    
+
     if score := heuristic_evaluation(board, player, maximize) != None:
         return score
 
-    if maximize:
-        enemy_player = 'X' if player == 'X' else 'O'
-        possible_states = generate_all_possible_states(board)
+    enemy_player = 'O' if player == 'X' else 'X'
+    possible_states = generate_all_possible_states(board, player)
+
+    if maximize:           
         max_score = - math.inf
         
         for state in possible_states:
-            max_score = max(min_max_tic_tac_toe(state, enemy_player, False), max_score)
+            potential_score = min_max_tic_tac_toe(state, enemy_player, False)
+            max_score = max(max_score, potential_score)
+
+        return max_score
             
     else:
+        min_score = math.inf
+        
+        for state in possible_states:
+            potential_score = min_max_tic_tac_toe(state, enemy_player, True)
+            min_score = min(potential_score, min_score)
 
+        return min_score
 
 table =[
     ['1', 'O', '.', '.'],
@@ -114,9 +132,9 @@ table3 =[
 ]
 
 table4 = [
-    ['1', 'X', 'O', 'X'],
-    ['2', 'O', '.', 'X'],
-    ['3', 'X', 'X', 'O'],
+    ['1', '.', '.', '.'],
+    ['2', '.', '.', '.'],
+    ['3', '.', '.', '.'],
     [' ', '1', '2', '3'],
 ]
 
@@ -124,4 +142,5 @@ table4 = [
 # print(is_terminal(table2))
 # print(is_terminal(table3))
 # print(heuristic_evaluation(table4, 'X', True))
-print(-1 == None)
+# print(-1 == None)
+print(generate_all_possible_moves(table4))
